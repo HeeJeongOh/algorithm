83930bf47bd9853a1d3d2589be1bc9868e325b81
gobi <ohhj1999@gmail.com>
2022-07-30 20:37:41 +0900

    이상한 문자 만들기 solved

--- programmers/p12930.py
+++ programmers/p12930.py
@@ -2,17 +2,17 @@
 # 단어 사이에 공백이 1개 이상
 def solution(s):
     answer = ''
-    n = 0
-    for letter in s:
-        if letter == ' ':
+    cnt = 0
+    for l in s:
+        if l == ' ':
             answer += ' '
-            n = 0
+            cnt = 0
         else:
-            if n % 2 == 0:
-                answer += letter.upper()
+            if cnt % 2 == 0:
+                answer += l.upper()
             else:
-                answer += letter.lower()
-            n += 1        
+                answer += l.lower()
+            cnt += 1
     return answer
 
 print(solution('try hello world'))
