7a1d8127cda448cfc5a13a5f8093fdf72016bd70
HeejeongOh <ohhj1999@gmail.com>
2023-03-17 14:04:40 +0900

    feat: LIMIT n

--- sql/131115.sql
+++ sql/131115.sql
@@ -1,12 +1,10 @@
--- SUCCESS
+/*
+select *
+from food_product
+where price = (SELECT max(price) as price from food_product);
+*/
 
 SELECT *
 FROM FOOD_PRODUCT
 ORDER BY PRICE DESC
 LIMIT 1;
-
-/* 다른 답안 
-select *
-from food_product
-where price = (SELECT max(price) as price from food_product);
-*/
