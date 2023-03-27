95a1923fd6f841a1f7f52fff82dfb45d3c894a28
HeejeongOh <ohhj1999@gmail.com>
2023-03-27 16:15:58 +0900

    카펫 solved

--- programmers/p42842.py
+++ programmers/p42842.py
@@ -1,32 +1,24 @@
 '''
-1.과거를 기억해야하면 스택 사용하기
-    1.1. 마지막 괄호가 ( 이면 바로 false 
-    1.2. ( 이면 push
-    1.3. ) 이면 이전 스택에 있는 것과 비교하여 닫히면 pop
-    
-[시도1] 
-    정확성: 50.2 - 런타임에러4 : 빈 스택인 경우 에외 처리, 실패1
-    효율성: 15.2
-[시도2]
-    정확성: 65.7 - 실패1 : 스택이 모두 비어있지 않은 경우에도 false 반환
-    효율성: 15.2 - 실패1
+- 갈색 테두리는 1줄, 노란색은 알 수 없음
+- 전체 카펫의 가로, 세로 크기
+0. 가로 > 세로
+1. 노란색 격자의 수를 통해 나올 수 있는 직사각형 조합 찾기
+    1.1 넓이 = 격자 수 -> 공약수 활용
+    1.2 공약수 구하는 방법 - 나머지 = 0
+2. 노란색의 가로, 세로를 통해 전체 둘레(갈색) 파악 가능
+    2.1 w = y_w + 2
+        h = y_h + 2
+    2.2 이때 그 조합들 중 w*h가 갈색 격자의 수와 매칭되는 것을 찾아야 함.
 '''
-def solution(s):
-    if s[-1] == '(':
-        return False
-    stack = []
-    for tmp in s:
-        if tmp == '(':
-            stack.append(tmp)
-        else:
-            # (+) try-catch와 IndexError 활용 가능.
-            if len(stack) == 0:
-                return False
-            if stack[-1] == '(':
-                stack.pop()
-                
-    # (+) return len(stack) == 0 으로 수정 가능.
-    if len(stack) != 0:
-        return False
-    
-    return True
+def solution(brown, yellow):
+    for i in range(1, int(yellow**(1/2))+1):
+        if yellow % i == 0:
+            y_w = yellow // i
+            y_h = i
+            
+            w = y_w + 2
+            h = y_h + 2
+
+            # sets.append([w, h])
+            if w*h == (brown+yellow):
+                return [w, h]
