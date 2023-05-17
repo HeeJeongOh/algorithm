d406a39a5816a03a06a3d7858ab3a56f067e26dc
HeejeongOh <ohhj1999@gmail.com>
2023-05-17 15:08:11 +0900

    기능개발 solved

--- programmers/p42586.py
+++ programmers/p42586.py
@@ -26,3 +26,33 @@ p = [93, 30, 55]
 s = [1, 30, 5]	
 r = [2, 1]
 print(solution(p, s) == r)
+
+'''
+0. 이전에 푼 문제 - 풀이 확인하지 않고 다시 품 -> :)
+[시도1] 90.9 / 100.0
+1. 차이 저장 
+2. 기준 숫자보다 크면 다음 배포에 포함시키기
+
+[시도2] 
+1. 차이 저장 시, int가 아닌 ceil 사용
+    ㄴ diff[30] -> 3
+'''
+import math
+def solution(progresses, speeds):
+    answer = []
+    diff = []
+    for p, s in zip(progresses, speeds):
+        diff.append(math.ceil((100-p)/s))
+        print(diff)
+
+    nstd = diff[0]
+    answer.append(0)
+    idx = 0
+    for d in diff:
+        if d > nstd:
+            nstd = d
+            answer.append(0)
+            idx += 1
+        answer[idx] += 1
+            
+    return answer
