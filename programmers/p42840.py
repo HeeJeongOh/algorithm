d05a9e6a4560761da880daa752626e5fbd8c6ac7
HeejeongOh <ohhj1999@gmail.com>
2023-05-17 12:58:25 +0900

    모의고사 solved

--- programmers/p42840.py
+++ programmers/p42840.py
@@ -23,3 +23,29 @@ def solution(answers):
             answer.append(i+1)
 
     return answer
+
+'''
+1. 이전에 풀었던 문제 - 수포자 규칙성 상수로 저장
+   ㄴ 규칙성이 없기 때문에 동일하게 푸는 게 베스트일 듯
+2. 이전보다 깔끔하게 정리해봄.
+'''
+
+def solution(answers):
+    student = [[],
+               [1, 2, 3, 4, 5],
+               [2, 1, 2, 3, 2, 4, 2, 5],
+               [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    scores = [-1, 0, 0, 0]
+    
+    for i, a in enumerate(answers):
+        if student[1][i % len(student[1])] == a:
+            scores[1] += 1
+        if student[2][i% len(student[2])] == a:
+            scores[2] += 1
+        if student[3][i % len(student[3])] == a:
+            scores[3] += 1
+    
+    nmax = max(scores)
+    answer = [i for i, s in enumerate(scores) if s == nmax]
+    
+    return answer
