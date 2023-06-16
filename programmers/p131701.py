34eae0c24df3e6d736a32f68233027fa50c2b9d5
HeejeongOh <ohhj1999@gmail.com>
2023-06-16 16:02:38 +0900

    연속 부분 수열 합의 개수 solved

--- programmers/p131701.py
+++ programmers/p131701.py
@@ -1,7 +1,6 @@
 '''
 원형수열 : 처음과 끝이 연결되어 있는 수열
 원형 수열의 연속 부분 수열 합 구하기
-'''
 def solution(elements):
     length = len(elements)
     elements += elements
@@ -13,7 +12,20 @@ def solution(elements):
             nums.append(sum(elements[i:i+l]))
     return len(set(nums))
 
-e = [7,9,1,1,4]	
-ans = 18
-
-print(solution(e))
+-- 230616
+1. 길이=i를 하나씩 늘려가면서 더하기
+    1.1 시작 인덱스가 다시 0으로 돌아오면 길이 늘리기
+2. 길이가 1일 때, 전체 길이일 때는 미리 더해두기
+3. 나머지로 할까 늘릴까 고민하다가 그냥 늘리는 게 덜 헷갈려서 그거 선택함
+'''
+def solution(elements):
+    answer = elements.copy()
+    answer += [sum(elements)]
+        
+    # 원형수열을 표현하기 위해 확장
+    size = len(elements)
+    elements += elements
+    for i in range(2, size):
+        for j in range(size):
+            answer.append(sum(elements[j:j+i]))
+    return len(set(answer))
