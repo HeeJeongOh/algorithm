2cd145abd926bfeaff06428c9a12c70974403d27
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-08-16 14:25:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,27 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-        int pointer = 0;
-        for(int i = 0; i < 10; i++){
-            if(pointer >= numbers.length){
-                for(int j = i; j < 10; j++){
-                    answer += j;
-                }
-                break;
-            }
-            // System.out.println(numbers[pointer] + " " + i);
-
-            if(numbers[pointer] != i){
-                answer += i;
-            }
-            else{
-                pointer += 1; 
-            }
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
         }
-        return answer;
+        return sum;
     }
 }
