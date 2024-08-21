cd7665d2ccfa7da149d3b01ef920ed2a33f19bdc
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-08-21 10:04:36 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.93 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -4,6 +4,7 @@ class Solution {
         for(int n = left; n < right+1; n++){
             // 약수 개수 더하기
             int cnt = 0;
+            // i <= (n/2)+1 으로 연산 줄이기 가능
             for(int i = 1; i <= n; i++){
                 if(n % i == 0){
                     cnt += 1;
