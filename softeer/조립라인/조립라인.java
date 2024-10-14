678d4771cdb20e83fd83f671f8dccb4a40fe5ae9
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-10-14 23:34:31 +0900

    PASS 조립라인.java

--- softeer/조립라인/조립라인.java
+++ softeer/조립라인/조립라인.java
@@ -37,11 +37,12 @@ public class Main {
         dp[0][0] = info[0][0];
         dp[0][1] = info[0][1];
         
-        for(int i = 1; i < N; i++){
+        for(int i = 1; i < N; i++){            
             // 0(A), 1(B), 2(A-B), 3(B-A)
             // 같은 조립라인을 더하는 것과 이전 조립라인+이동시간을 더하는 것 중에 고르기
-            dp[i][0] = dp[i-1][0] + Math.min(info[i][0], info[i-1][1] + info[i][3]);
-            dp[i][1] = dp[i-1][1] + Math.min(info[i][1], info[i-1][0] + info[i][2]);
+            dp[i][0] = info[i][0] + Math.min(dp[i-1][0], dp[i-1][1] + info[i-1][3]);
+            dp[i][1] = info[i][1] + Math.min(dp[i-1][1], dp[i-1][0] + info[i-1][2]);
+            // System.out.println(Arrays.toString(dp[i]));
         }
         
         System.out.println(Math.min(dp[N-1][0], dp[N-1][1]));
