908f09909856cc34e0ca3f8b6d6a6a9bd8d277f6
HEEJEONG OH <62127798+HeeJeongOh@users.noreply.github.com>
2024-10-23 18:09:30 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 20.71 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,45 +1,28 @@
 import java.util.*;
-/*
-[힌트] 투포인터
 
-*/
 class Solution {
     public int[] solution(int[] sequence, int k) {
         int[] answer = {};
         int len = sequence.length;
-        int start = 0;
-        int end = 0;
-        
+        int start = 0, end = 0;
         int sum = sequence[0];
-        while(start < len && end < len){
-            if(sum < k){
-                end += 1;
-                
-                if(end < len) {  // 배열 범위 체크
-                    sum += sequence[end];
-                }
-            }
-            else if(sum > k){
-                sum -= sequence[start];
-                start += 1;
-            }
-            else{
-                if(answer.length > 0){
-                    if(answer[1] - answer[0] > end-start){
-                        answer = new int[]{start, end};
-                    }
-                }else{
+        
+        while (start < len && end < len) {
+            if (sum == k) {
+                if (answer.length == 0 || (end - start) < (answer[1] - answer[0])) {
                     answer = new int[]{start, end};
                 }
-                
-                // end 포인터 증가
-                end += 1;
-                if(end < len) {  // 배열 범위 체크
-                    sum += sequence[end];
-                }
             }
-            // System.out.println(start + "-" + end + " : " + sum);
+
+            if (sum <= k) {
+                end++;
+                if (end < len) sum += sequence[end];
+            } else {
+                sum -= sequence[start];
+                start++;
+            }
         }
+
         return answer;
     }
 }
